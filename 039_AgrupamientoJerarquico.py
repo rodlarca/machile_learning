3e66a98204fa52cacaf89d0e0f6702df979f5a97
rodlarca <rodlarca@gmail.com>
2025-11-12 23:52:46 -0300

    add agrupamiento jerárquico

---
+++ 039_AgrupamientoJerarquico.py
@@ -0,0 +1,22 @@
+# Importar las librerías necesarias
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_blobs
+from scipy.cluster.hierarchy import dendrogram, linkage
+from sklearn.cluster import AgglomerativeClustering
+
+# Generar un dataset sintético
+X, _ = make_blobs(n_samples=500, centers=4, cluster_std=0.60, random_state=0)
+
+# Crear el modelo de agrupamiento jerárquico aglomerativo
+model = AgglomerativeClustering(n_clusters=None, distance_threshold=0)
+
+# Ajustar el modelo
+model.fit(X)
+
+# Generar el dendrograma
+Z = linkage(X, method='ward')
+plt.figure(figsize=(10, 7))
+dendrogram(Z)
+plt.title("Dendrograma de Agrupamiento Jerárquico")
+plt.show()
